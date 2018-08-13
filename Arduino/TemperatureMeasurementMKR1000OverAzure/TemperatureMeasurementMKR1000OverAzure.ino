4a073411e75b11a4b1100f356d123ab0b4322442
Serge Desmedt <sergedesmedt@yahoo.com>
2018-08-13 22:00:36 +0200

    Arduino dynamic WIFI configuration

--- Arduino/TemperatureMeasurementMKR1000OverAzure/TemperatureMeasurementMKR1000OverAzure.ino
+++ Arduino/TemperatureMeasurementMKR1000OverAzure/TemperatureMeasurementMKR1000OverAzure.ino
@@ -10,6 +10,9 @@
 static char ssid[] = IOT_CONFIG_WIFI_SSID;
 static char pass[] = IOT_CONFIG_WIFI_PASSWORD;
 
+String ssidString;
+String passString;
+
 const int SensorPin = A0;
 const unsigned long SendEveryMilliSeconds = 500;
 
@@ -20,11 +23,34 @@ int sensorValue;
 float voltageValue;
 float temperatureValue;
 
+
+int wifiConnectLedPin = 6;
+int wifiConnectLedState = LOW;
+
+int serialConnectLedPin = 1;
+int serialConnectLedState = LOW;
+
+int receivingBlinkTimeOut = 5;
+
+bool receivedWifiNetwork = false;
+bool receivedWifiPwd = false;
+const int maxWifiConnectAttempts = 10;
+int wifiConnectAttempt = 0;
+int status = WL_IDLE_STATUS;
+
 void setup() {
 
-  sample_init(ssid, pass);
+    Serial.begin(9600);  
+
+    pinMode(serialConnectLedPin, OUTPUT);
+    pinMode(wifiConnectLedPin, OUTPUT);
+    
+    digitalWrite(serialConnectLedPin, serialConnectLedState);
+    digitalWrite(wifiConnectLedPin, wifiConnectLedState);
+
+//  sample_init(ssid, pass);
   
-//    int status = WL_IDLE_STATUS;
+    
 //    Serial.print("Attempting to connect to WPA SSID: ");
 //    Serial.println(ssid);
 //
@@ -45,16 +71,109 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  currentTime = millis();
 
-  if ((currentTime - lastMeasurementTime) > SendEveryMilliSeconds)
-  {
-    CalcTemperature(&sensorValue, &voltageValue, &temperatureValue);
-    //SendToSerial(sensorValue, voltageValue, temperatureValue);
-    SendToAzure(sensorValue, voltageValue, temperatureValue);
-    //SendToAzure(10, 20, 30);
-    lastMeasurementTime = millis();
-  }
+    char receiveVal; 
+    String receivedString;    
+     
+    if(Serial.available() > 0)  
+    {          
+       receivedString = Serial.readString();
+
+       if(!receivedWifiNetwork && !receivedWifiPwd)
+       {
+          //ssidString = new char [receivedString.length()+1];
+          //strcpy (ssidString, receivedString.c_str());
+          ssidString = receivedString;
+
+          receivedWifiNetwork = true;
+       }
+
+       if(receivedWifiNetwork && !receivedWifiPwd)
+       {
+          //passString = new char [receivedString.length()+1];
+          //strcpy (passString, receivedString.c_str());
+          passString = receivedString;
+
+          receivedWifiPwd = true;
+       }
+       
+       if(receivedWifiNetwork && receivedWifiPwd)      
+          serialConnectLedState = HIGH;    
+
+       digitalWrite(serialConnectLedPin, serialConnectLedState); 
+       Serial.println(receivedString);
+       Serial.flush();
+    }  
+
+
+    while ((status != WL_CONNECTED) && receivedWifiNetwork && receivedWifiPwd) 
+    {
+        Serial.println("Not yet conected to WIFI");
+        
+        Serial.println("WiFi " + String(ssid));
+        //Serial.println(ssidString);
+        Serial.flush();
+
+        Serial.print("WiFi pwd " + String(pass));
+        //Serial.println(passString);
+        Serial.flush();
+        
+        // Connect to WPA/WPA2 network:
+        status = WiFi.begin(ssid, pass);
+        Serial.println("WiFi status: " + ((status == WL_CONNECTED) ? String("connected") : String("not connected")));
+        //Serial.println((status == WL_CONNECTED) ? "connected" : "not connected");
+        Serial.flush();
+        
+        Serial.print("Try again...");
+        Serial.flush();
+
+        // wait 2 seconds to try again
+        delay(2000);
+
+
+        wifiConnectAttempt++;
+
+        Serial.println("Attemp: " + String(wifiConnectAttempt));
+        //Serial.println(String(wifiConnectAttempt));
+        Serial.flush();
+          
+        if(wifiConnectAttempt > maxWifiConnectAttempts)
+        {
+          Serial.println("Maximum attempts for wifi connection exceeded.");
+          Serial.flush();
+          break;
+        }
+    }
+
+    if((status == WL_CONNECTED) && receivedWifiNetwork && receivedWifiPwd)
+    {
+      wifiConnectLedState = HIGH;   
+      digitalWrite(wifiConnectLedPin, wifiConnectLedState); 
+      IPAddress ip = WiFi.localIP();
+      Serial.println("Conected to WIFI: " + ip);
+      Serial.flush();
+    }
+    else
+    {
+      serialConnectLedState = LOW;  
+      digitalWrite(serialConnectLedPin, serialConnectLedState); 
+    }
+      
+        
+    delay(50); 
+  
+  
+
+//  currentTime = millis();
+//
+//  if ((currentTime - lastMeasurementTime) > SendEveryMilliSeconds)
+//  {
+//    CalcTemperature(&sensorValue, &voltageValue, &temperatureValue);
+//    //SendToSerial(sensorValue, voltageValue, temperatureValue);
+//    SendToAzure(sensorValue, voltageValue, temperatureValue);
+//    //SendToAzure(10, 20, 30);
+//    lastMeasurementTime = millis();
+//  }
   
 }
 
---
+++ Arduino/VisualStudio/ArduinoConfigurator/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string ardiuinoSerialPort = AutodetectArduinoPort();
+            if (string.IsNullOrEmpty(ardiuinoSerialPort))
+                return;
+
+            SerialPort port = new SerialPort(ardiuinoSerialPort, 9600); //, Parity.None, 8, StopBits.One);
+            port.Open();
+            port.DataReceived += Port_DataReceived;
+
+            port.WriteLine(TxtWifiNtwrk.Text);
+            port.WriteLine(TxtWifiPwd.Text);
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort spL = (SerialPort)sender;
+            //const int bufSize = 12;
+            //Byte[] buf = new Byte[bufSize];
+            //Console.WriteLine("DATA RECEIVED!");
+            //Console.WriteLine(spL.Read(buf, 0, bufSize));
+            Console.WriteLine(spL.ReadLine());
+        }
+
+        // https://stackoverflow.com/questions/3293889/how-to-auto-detect-arduino-com-port
+        private string AutodetectArduinoPort()
+        {
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+
+            try
+            {
+                foreach (ManagementObject item in searcher.Get())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        return deviceId;
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+                /* Do Nothing */
+            }
+
+            return null;
+        }
+    }
+}
