713d04133d3bb3b1a317b81c4482d02f53bc12b8
Serge Desmedt <sergedesmedt@yahoo.com>
2018-09-04 21:28:44 +0200

    Arduino switch device configuration

---
+++ Arduino/TemperatureMeasurementMKR1000OverAzure/DeviceConfig.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "DeviceConfig.h"
+
+void DeviceConfig() {
+  String descriptor = "<subsystems>"
+  "<system id='1'>"
+    "<ui>"
+      "<toggle>"
+        "<action event='on' method='1_on'/>"
+        "<action event='off' method='1_off'/>"
+      "</toggle>"
+    "</ui>"
+  "</system>"
+  "<system id='2'>"
+    "<ui>"
+      "<toggle>"
+        "<action event='on' method='2_on'/>"
+        "<action event='off' method='2_off'/>"
+      "</toggle>"
+    "</ui>"
+  "</system>"
+  "</subsystems>";
+}
---
+++ Arduino/TemperatureMeasurementMKR1000OverAzure/DeviceConfig.h
@@ -0,0 +1,6 @@
+#ifndef DEVICECONFIG_H
+#define DEVICECONFIG_H
+
+void DeviceConfig();
+
+#endif /* DEVICECONFIG_H */
